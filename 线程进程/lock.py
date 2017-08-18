b2ab4ea50768e0004d4443d360fcf6cae52338f2
Beck <by66666@gmail.com>
2017-08-18 13:53:15 +0800

    线程锁, 用来防止多线程同时操作同一变量

--- 线程进程/lock.py
+++ 线程进程/lock.py
@@ -3,6 +3,7 @@ import threading
 
 # 假定银行存款
 balance = 0
+lock = threading.Lock()
 
 def change_it(n):
     # 先存后取, 结果应该为0:
@@ -12,7 +13,13 @@ def change_it(n):
 
 def run_thread(n):
     for i in range(100000):
-        change_it(n)
+        # 先要获取锁:
+        lock.acquire()
+        try:
+            change_it(n)
+        finally:
+            # 释放锁
+            lock.release()
 
 
 t1 = threading.Thread(target=run_thread, args=(5, ))

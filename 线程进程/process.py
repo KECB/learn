62e7570b3e2b7dbf08f6e2255c96b2f97e74dccc
Beck <by66666@gmail.com>
2017-08-18 11:41:10 +0800

    子进程

--- 线程进程/process.py
+++ 线程进程/process.py
@@ -3,6 +3,7 @@ import time
 import random
 from multiprocessing import Process
 from multiprocessing import Pool
+import subprocess
 
 
 # 子进程要执行的代码
@@ -19,16 +20,25 @@ def long_time_task(name):
 
 
 if __name__ == '__main__':
-    print('Parent process %s.' % os.getpid())
+    # print('Parent process %s.' % os.getpid())
     # p = Process(target=run_proc, args=('test', '1', ))
     # print('Child process will start.')
     # p.start()
     # p.join()
     # print('Child process end.')
-    p = Pool(17)
-    for i in range(18):
-        p.apply_async(long_time_task, args=(i, ))
-    print('Waiting for all subprocesses done...')
-    p.close()
-    p.join()
-    print('All subprocesses done.')
+    # p = Pool(17)
+    # for i in range(18):
+    #     p.apply_async(long_time_task, args=(i, ))
+    # print('Waiting for all subprocesses done...')
+    # p.close()
+    # p.join()
+    # print('All subprocesses done.')
+    print('$ nslookup www.python.org')
+    r = subprocess.call(['nslookup', 'www.python.org'])
+    print('Exit code:', r)
+    print('$ nslookup')
+    p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+    print(output.decode('utf-8'))
+    print('Exit code:', p.returncode)

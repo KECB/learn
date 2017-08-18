b2356cd2491385ded4a5847f14e292d0949aacc8
Beck <by66666@gmail.com>
2017-08-18 11:50:40 +0800

    进程通信

--- 线程进程/process.py
+++ 线程进程/process.py
@@ -3,6 +3,7 @@ import time
 import random
 from multiprocessing import Process
 from multiprocessing import Pool
+from multiprocessing import Queue
 import subprocess
 
 
@@ -19,6 +20,23 @@ def long_time_task(name):
     print('Task %s runs %0.2f seconds.' % (name, end - start))
 
 
+# 写数据进程
+def write(q):
+    print('Process to write: %s' % os.getpid())
+    for value in ['A', 'B', 'C', 'D']:
+        print('Put %s to queue...' % value)
+        q.put(value)
+        time.sleep(random.random())
+
+
+# 度数据进程
+def read(q):
+    print('Process to read: %s' % os.getpid())
+    while True:
+        value = q.get(True)
+        print('Get %s from queue.' % value)
+
+
 if __name__ == '__main__':
     # print('Parent process %s.' % os.getpid())
     # p = Process(target=run_proc, args=('test', '1', ))
@@ -33,12 +51,26 @@ if __name__ == '__main__':
     # p.close()
     # p.join()
     # print('All subprocesses done.')
-    print('$ nslookup www.python.org')
-    r = subprocess.call(['nslookup', 'www.python.org'])
-    print('Exit code:', r)
-    print('$ nslookup')
-    p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE,
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
-    print(output.decode('utf-8'))
-    print('Exit code:', p.returncode)
+
+    # print('$ nslookup www.python.org')
+    # r = subprocess.call(['nslookup', 'www.python.org'])
+    # print('Exit code:', r)
+    # print('$ nslookup')
+    # p = subprocess.Popen(['nslookup'], stdin=subprocess.PIPE,
+    #                      stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # output, err = p.communicate(b'set q=mx\npython.org\nexit\n')
+    # print(output.decode('utf-8'))
+    # print('Exit code:', p.returncode)
+
+    # 父进程创建 Queue, 并传给各个子进程:
+    q = Queue()
+    pw = Process(target=write, args=(q, ))
+    pr = Process(target=read, args=(q, ))
+    # 启动子进程 pw, 写入:
+    pw.start()
+    # 启动子进程 pr, 读取:
+    pr.start()
+    # 等待 pw 结束
+    pw.join()
+    # pr 进程里是死循环, 无法等待其结束, 只能强行终止:
+    pr.terminate()
